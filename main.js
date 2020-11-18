ca3a38f95862f40a2156236cab6d0ccc3c805b78
Andrew Theiss <andrew.theiss@gmail.com>
2020-11-18 09:31:51 -0800

    Added game Div content as per tech spec

--- main.js
+++ main.js
@@ -2,17 +2,8 @@ function unloadContent() {
   // Using jquery
   $("#main_container").html('');
 
-  // Using Javascript
-  document.getElementById("main_container").innerHTML = "";
-
   // Add style or remove style
   $("#main_container").removeClass("choco-style");
-}
-
-
-// ==================================================================
-function loadContentForChocolate(result) {
-  document.getElementById("demo").innerHTML = "Hello World!";
 }
 
 function loadContent(result) {
@@ -20,15 +11,11 @@ function loadContent(result) {
   $("#main_container").html(result);
 }
 
-
 function loadContentChocolate(result) {
   unloadContent();
   $("#main_container").html(result);
   $("#main_container").addClass("choco-style");
 }
-
-
-
 
 function requestChocolateContent() {
   unloadContent();
@@ -44,6 +31,17 @@ function requestChocolateVisuals() {
   unloadContent();
   $.ajax({url: "/pages/visuals.html", success: loadContent});
 }
+
+function loadGame (htmlContent) {
+  $("#game_content").html(htmlContent);
+}
+
+function requestGame() {
+  $.ajax({url: "/pages/game.html", success: loadGame});
+}
+
+
+
 function requestHome() {
   unloadContent();
   $.ajax({url: "/pages/splash.html", success: loadContent});
@@ -59,5 +57,6 @@ document.getElementById("go_to_hw").onclick = function () {
 $("#load_chocolate").click(requestChocolateContent);
 $("#load_meditation").click(requestChocolateMeditation);
 $("#load_visuals").click(requestChocolateVisuals);
+$("#load_game").click(requestGame);
 $("#home").click(requestHome);
 // Document 'ready' function is called when all content is loaded
