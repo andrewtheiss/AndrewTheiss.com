8bda88a2d249a21086d55a166999ac3de7ace770
Andrew Theiss <andrew.theiss@gmail.com>
2021-01-05 20:45:46 -0800

    Add nav outline

---
+++ src/chocolate/chocolate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './chocolate.css';
+
+class Chocolate extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default Chocolate;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
+import Chocolate from './chocolate/chocolate.js';
 import reportWebVitals from './reportWebVitals';
 import firebase from 'firebase/app';
 
