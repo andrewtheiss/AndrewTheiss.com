4381e01780b4b1064626de111c4be96cc9cbfc9f
Andrew Theiss <andrew.theiss@gmail.com>
2022-04-02 00:30:38 -0700

    Try and Grab list of transactions

--- src/components/Taxes/Overview/Pages/OverviewMainPage.js
+++ src/components/Taxes/Overview/Pages/OverviewMainPage.js
@@ -5,7 +5,7 @@ class TaxesOverviewMainPage extends React.Component {
   constructor(props) {
     super(props);
     this.defaultWallet = 'theiss.eth';
-
+    this.etherscanApi = 'NCTXRETEMIUZU5FJTDZHHV43DE99699FDP';
     this.grabTransactionList = this.grabTransactionList.bind(this);
 
     this.state = {
@@ -17,20 +17,7 @@ class TaxesOverviewMainPage extends React.Component {
   }
 
   // Get data from DB in this function
-  componentDidMount = () => {
-    /*const beanCollectionRef = this.props.firebase.db.collection("beans");
-    let self = this;
-    beanCollectionRef.get().then(function(beanCollectionDocs) {
-      var beansMap = {};
-      beanCollectionDocs.forEach(function(doc) {
-        beansMap[doc.id] = doc.data();
-      });
-
-      self.setState({
-        beans : beansMap
-      });
-    });*/
-
+  async componentDidMount() {
     let eth = {
       network : "mainnet",
       provider : null,
@@ -38,26 +25,41 @@ class TaxesOverviewMainPage extends React.Component {
     };
 
     // API specific key
-    eth.provider = ethers.getDefaultProvider(eth.network, {
-        etherscan: 'NCTXRETEMIUZU5FJTDZHHV43DE99699FDP'
+    eth.provider = await ethers.getDefaultProvider(eth.network, {
+        etherscan: this.etherscanApi
     });
 
     //ethers.provider = new ethers.providers.Web3Provider(window.ethereum);
     eth.ethers = ethers;
+    var transactionList = this.grabTransactionList(eth.provider);
     this.setState({eth});
-    //this.grabTransactionList(null);
   }
 
 
-  grabTransactionList(grabTransactionList) {
+  async grabTransactionList(provider) {
 
     let transactionDetails = {
       count : null
     };
     // How many transactions do we have?
-    transactionDetails.count = this.state.eth.provider.getTransactions(this.defaultWallet);
+    //https://etherscan.io/exportData?type=address&a=0x3afb0b4ca9ab60165e207cb14067b07a04114413
+    transactionDetails.wallet = await provider.resolveName(this.defaultWallet);
+    transactionDetails.count = await provider.getTransactionCount(this.defaultWallet);
     console.log('found ' + transactionDetails.count);
 
+    const response = await fetch(`https://api.etherscan.io/api?module=account
+         &action=txlist
+         &address=` + transactionDetails.wallet + `
+         &startblock=0
+         &endblock=99999999
+         &page=1
+         &offset=10
+         &sort=asc
+         &apikey=` + this.etherscanApi
+       );
+    const json = await response.json();
+    console.log(json);
+    this.setState({ data: json });
     // Grab transactions from database and check
 
   }
