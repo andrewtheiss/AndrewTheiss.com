c8bd0510f8f72e44b65863d064f61b4705afd5f6
Andrew Theiss <andrew.theiss@gmail.com>
2022-04-05 06:04:47 -0700

    Work on getting the gas price based on whitelist transaction array

---
+++ src/components/Taxes/Overview/Transactions/Consts.js
@@ -0,0 +1,5 @@
+export const OWNED_ACCOUNTS_LIST = [
+  '0x3afb0b4ca9ab60165e207cb14067b07a04114413', // theiss.eth
+  '0x6845dd13e4881C346b3D2E9953f91e72aB17B467', // stabletheiss.eth
+  '0xB09b....2863', // bnb account
+]
--- src/components/Taxes/Overview/Transactions/Processor.js
+++ src/components/Taxes/Overview/Transactions/Processor.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import { ethers } from "ethers";
+import * as CONSTS from './Consts.js'
 
 class TransactionProcessor extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this, props);
+    this.processTransaction = this.processTransaction.bind(this);
+    this.calcTxnCost = this.calcTxnCost.bind(this);
+    console.log(props);
   }
 
   processTransaction() {
-    console.log('CALL TO PROCESSED TRANSASCTION');
+
+    let price = this.calcTxnCost();
+
+
+    this.state = {
+      index : '', // Same as block number?
+      blockNumber : this.props.blockNumber,
+      order : '', // Probably not used but there could be 2 transactions in the same block...
+      txnHash : '',
+      timestamp : '',
+      gasCost : price, // This doubles as determining if it's to/from
+      contractInteraction : '',
+
+      // Heavy lifting here, but get other parts to work first
+      input : '',
+      processedInput : {},
+      processed : false
+    };
+
+    console.log(this.state);
+    return this.state;
+  }
+
+  calcTxnCost() {
+    let txnCost = 0;
+
+    for (var i = 0; i < CONSTS.OWNED_ACCOUNTS_LIST.length; i++) {
+      if (CONSTS.OWNED_ACCOUNTS_LIST[i] == this.props.from) {
+        // Non-zero cost
+        txnCost = this.props.effectiveGasPrice._hex;
+      }
+    }
+
+    if (txnCost != 0) {
+      alert('txn cost' + txnCost);
+    }
+    // We need to have a list of all txns where the sender is one of our addresses
+    return txnCost;
   }
 }
 
